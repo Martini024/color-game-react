0f2d371ac0690ddb2f6dcf6c8ff711e821c2e23a
martini <zhao435021640@gmail.com>
2019-02-09 22:38:20 +0800

    v0.0 integret react-bootstrap and finish the basic setup
    Co-Authored-By: null <Martini024@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Jumbotron } from 'react-bootstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (<Jumbotron>
+            <h1>Hello, world!</h1>
+            <p>
+                This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+            </p>
+            <p>
+                <Button variant="primary">Learn more</Button>
+            </p>
+        </Jumbotron>);
+    }
 }
 
 export default App;
