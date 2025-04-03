474ffa1298df6731676602b01c512d2191a8145a
Martini <martini.zhao@gmail.com>
2025-04-03 22:48:36 +0800

    Updates

---
+++ precache-manifest.5f74fab1aeccbca339bc8a07a1d43eba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d984d696b7c8cea4c69",
+    "url": "/color-game-react/static/css/main.891cf39f.chunk.css"
+  },
+  {
+    "revision": "1d984d696b7c8cea4c69",
+    "url": "/color-game-react/static/js/main.1d984d69.chunk.js"
+  },
+  {
+    "revision": "19b1bc68ce5bb6c3f725",
+    "url": "/color-game-react/static/js/1.19b1bc68.chunk.js"
+  },
+  {
+    "revision": "5b03e12252c92cd0e715",
+    "url": "/color-game-react/static/js/runtime~main.5b03e122.js"
+  },
+  {
+    "revision": "ddeafee37f4e5ac8d4fe65a6e5f86d3a",
+    "url": "/color-game-react/index.html"
+  }
+];
